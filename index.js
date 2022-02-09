f52d12a323f3aaab8c32dd1250479b52454da20d
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-02-09 22:17:48 +0100

    Started the new design

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require("path");
+const favicon = require('serve-favicon');
+
+const port = 8000;
+const url = "http://localhost:"+port;
+const app = express();
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {cors: { origin: "*"}})
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, '/views'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "html");
+
+app.get('/', (req, res) => {
+  res.render("index.ejs")
+})
+
+app.get('/luki', (req, res) => {
+  res.render("luki.ejs")
+})
+
+io.on('connection', (socket) => {
+  console.log(socket.id + " connected");
+  socket.on("disconnect", () => {
+    console.log(socket.id + " disconnected")
+  })
+
+  socket.on('message', (msg) => {
+    socket.broadcast.emit('message', msg)
+  })
+})
+
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
