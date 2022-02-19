8a3c31845554c1396fb4692fd984a47dfd62ce6c
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-02-20 00:40:15 +0100

    nice try by t-ferrarahh
    Co-Authored-By: ForKio <55454285+TFerrarah@users.noreply.github.com>

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {cors: { origin: "*"}})
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 app.set('views', path.join(__dirname, '/views'));
 app.set("view engine", "ejs");
 
